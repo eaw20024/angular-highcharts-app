76e2ec2bdd7ebb98a9d74f50f3f33c1a6d89ee80
Eric Washington <ericwashington@erics-mbp.eia.doe.gov>
2018-03-14 10:03:34 -0400

    Initial Commit

---
+++ angular-highcharts-app/src/app/components/chart-demo/simplechart/simplechart.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts'; 
+
+@Component({
+  selector: 'app-simplechart',
+  templateUrl: './simplechart.component.html',
+  styleUrls: ['./simplechart.component.css']
+})
+export class SimplechartComponent implements OnInit {
+
+  chart = new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'Line 1',
+      data: [1, 2, 3]
+    }]
+  });
+
+  // add point to chart serie
+  add() {
+    this.chart.addPoint(Math.floor(Math.random() * 10));
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
